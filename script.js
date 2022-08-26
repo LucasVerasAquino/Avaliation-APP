3dd80a27ee2072352f3136f04418f98429a3589f
Lucas Veras <lucasverasaquino@gmail.com>
2022-08-26 09:05:39 -0300

    Reajustes

---
+++ script.js
@@ -0,0 +1,20 @@
+//get rating options
+const ratingOpts = document.querySelectorAll(".rating-btn")
+
+
+
+function rateSelected() {
+    const rate = document
+
+    
+}
+
+
+
+
+//click on rating option
+ratingOpts.forEach(function(rate){
+
+    rate.addEventListener("click", rateSelected)
+    
+})
